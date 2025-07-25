b31490c7c7506eb524238166721c40f9b998c648
shreyasdevadiga <sdevadigashreya@gmail.com>
2025-07-25 08:52:30 +0000

    app changedd

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def use_pandas():
     df = pd.DataFrame(data)
     return df.to_json(orient='records')
 
-if _name_ == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
